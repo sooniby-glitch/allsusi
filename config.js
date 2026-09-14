8a9aa5a5e05d00bd9238dc5b874a649dfcfc6dca
sooniby-glitch <sooniby@gmail.com>
2026-09-14 11:48:57 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Apps Script 배포 후 나오는 웹앱 URL을 아래에 붙여넣으세요.
 // 예: https://script.google.com/macros/s/AKfycbx.../exec
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzyLERjVeiESyPYOP4DwiGy6YkFrBtctxIZyWxo_VtMGQToXV7yVBzYv9g4kSGJKDUn6Q/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwFtqla9UVaT0sFpWVHKyHzlEZglKPKITSU2THji9ZlH5Dn9yjoz_XhwJV9-8THnF50/exec';
